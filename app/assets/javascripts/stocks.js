1da58f304fd23a2e3afccdf022e4f540194c319a
By-CheChe-K <baichechekeii@gmail.com>
2016-12-09 17:03:40 +0900

    Add friendhip and friend search functionality

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,6 +1,6 @@
 var init_stock_lookup;
 
-init_stock_lookup = function(){
+  init_stock_lookup = function(){
   $('#stock-lookup-form').on('ajax:before', function(event, data, status){
     show_spinner();
   });
@@ -11,12 +11,12 @@ init_stock_lookup = function(){
 
   $('#stock-lookup-form').on('ajax:success', function(event, data, status){
     $('#stock-lookup').replaceWith(data);
-  init_stock_lookup();
+    init_stock_lookup();
   });
 
-  $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error) {
+  $('#stock-lookup-form').on('ajax:error', function(event, xhr, status, error){
     hide_spinner();
-    $('#stock-lookup-results').replaceWith(' ')
+    $('#stock-lookup-results').replaceWith(' ');
     $('#stock-lookup-errors').replaceWith('Stock was not found.');
   });
 }
